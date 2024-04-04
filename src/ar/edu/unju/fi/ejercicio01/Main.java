4cd1b628802a63485b0ca123b3210227c08450bc
valer <valer@Hernn>
2024-04-04 15:55:13 -0300

    Inicio del Proyecto Tp01

---
+++ src/ar/edu/unju/fi/ejercicio01/Main.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.ejercicio01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float primerNumero = 8;
+		float segundoNumero = 5;
+		float tercerNumero = 1;
+		float cuartoNumero = 20;
+		float quintoNumero = 9;
+		float promedio = (primerNumero + segundoNumero + tercerNumero + cuartoNumero + quintoNumero)/ 5;
+		System.out.println(primerNumero+", "+segundoNumero+", "+tercerNumero+", "+cuartoNumero+", "+quintoNumero);
+		System.out.println("El promedio es: "+ promedio );
+		
+	}
+
+}
